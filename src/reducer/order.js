8085a2c8294208e5845b622af2a5b1885c7ea416
1481980097@qq.com <1481980097@qq.com>
2019-05-05 14:07:57 +0800

    增加order reducer

--- src/reducer/order.js
+++ src/reducer/order.js
@@ -9,6 +9,15 @@ export default (state = initState, action) => {
     case types.ORDER_FINDORDERSUCCESS:
       //把查到的订单信息放进store里面
       return { ...state, items: action.data };
+    case types.ORDER_ADDNEWORDERSUCCESS:
+      return { ...state, items: [...state.items, action.data] };
+    case types.ORDER_DELETEORDERSUCCESS:
+      return {
+        ...state,
+        items: state.items.filter(v => {
+          return v.id !== action.data.id;
+        })
+      };
     default:
       return state;
   }
